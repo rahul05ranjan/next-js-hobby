24360a45081cbe8cbbc40aa09d3445ff9775217b
rahul05ranjan <rahulsingh.ranjan06@gmail.com>
2022-05-18 22:46:36 +0530

    adding pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
+const withPWA = require("next-pwa");
+
 // next.config.js
 module.exports = {
     images: {
       domains: ['avatars.githubusercontent.com'],
     },
   }
+
+  module.exports = withPWA({
+    pwa: {
+      dest: "public",
+      register: true,
+      skipWaiting: true,
+    },
+  });
